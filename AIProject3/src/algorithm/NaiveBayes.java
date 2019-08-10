6372ffb33324a2a4c50c17c1b3f75555db4057f2
Charmian Goh <csg102@rutgers.edu>
2019-08-09 20:25:16 -0400

    separated values in naive bayes into two grids

--- AIProject3/src/algorithm/NaiveBayes.java
+++ AIProject3/src/algorithm/NaiveBayes.java
@@ -13,14 +13,19 @@ public class NaiveBayes {
 	}
 
 	public void likelihood(Cell[][] image, double label_quantity, int k){
+		double[][] empty_grid = new double[image.length][image.length];
+		double[][] nonempty_grid = new double[image.length][image.length];
 
 		//array is already in 0 and 1 form
 		for(int digit = 0; digit < 10; digit++ ){
 			for(int i = 0; i < 28; i++){
 				for(int j = 0; j < 28; j++){
-					if(image[i][j].getData() != 0){
+					if(image[i][j].getData() == 0){
 						likelihood[digit][i][j]++;
-						image[i][j].updateBelief(likelihood[digit][i][j]);
+						empty_grid[i][j] = likelihood[digit][i][j];
+					}else{
+						likelihood[digit][i][j]++;
+						nonempty_grid[i][j] = likelihood[digit][i][j];
 					}
 				}
 			}
@@ -31,7 +36,11 @@ public class NaiveBayes {
 				for(int j = 0; j < 28; j++){
 					//check parentheses to see if they are ok
 					double belief = (likelihood[digit][i][j] + k)/(frequency[i] + Math.pow(k, 2));
-					image[i][j].updateBelief(belief);
+					if(image[i][j].getData() == 0){
+						empty_grid[i][j] = belief;
+					}else{
+						nonempty_grid[i][j] = belief;
+					}
 				}
 			}
 		}
