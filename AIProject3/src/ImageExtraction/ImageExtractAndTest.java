d47cfcd2aabb9b94b68477c2cb23a458f1831f92
Charmian Goh <csg102@rutgers.edu>
2019-08-10 00:40:24 -0400

    NaiveBayes complete but algorithm for testing needs help, can currently test for accuracy

--- AIProject3/src/ImageExtraction/ImageExtractAndTest.java
+++ AIProject3/src/ImageExtraction/ImageExtractAndTest.java
@@ -17,6 +17,17 @@ public class ImageExtractAndTest {
 		NaiveBayes nb = new NaiveBayes(extract.getDigit_frequency(),
 				Digit.getPrior(extract.getDigit_frequency(), extract.getTotal()), 1, digits);
 
+		ArrayList<Digit> digits_test = extract.digitExtract(ImageExtraction.Type.DIGIT_TEST, new double[10], 0);
+		int matched = 0;
+		for(int i = 0;  i < digits_test.size(); i++){
+			boolean match = nb.testDigit(digits.get(i), 1);
+			if(match){
+				matched++;
+			}
+		}
+		System.out.println(matched);
+		System.out.println(digits_test.size());
+		System.out.println("accuracy: " + (matched*1.0)/digits_test.size());
 	}
 
 }
--- AIProject3/src/algorithm/NaiveBayes.java
+++ AIProject3/src/algorithm/NaiveBayes.java
@@ -1,6 +1,5 @@
 package algorithm;
 
-import java.text.DecimalFormat;
 import java.util.ArrayList;
 
 import objects.Cell;
@@ -10,7 +9,6 @@ public class NaiveBayes {
 	private double[][][] likelihood;
 
 	private double[] frequency, priors;
-	private ArrayList<Digit> digits;
 
 	public NaiveBayes(double[] quantity, double[] priors, int k, ArrayList<Digit> digits){
 		//instantiate
@@ -18,7 +16,6 @@ public class NaiveBayes {
 
 		this.frequency = quantity;
 		this.priors = priors;
-		this.digits = digits;
 
 		//get total likelihoods
 		for(int i = 0 ; i < digits.size(); i++){
@@ -41,16 +38,40 @@ public class NaiveBayes {
 				}
 			}
 		}
-
-		//testing
-		/*
-		DecimalFormat df = new DecimalFormat("###.##");
-		for(int i = 0; i < 28; i++){
-			for(int j = 0; j < 28; j++){
-				System.out.print(df.format(likelihood[0][i][j]) + " ");
-			}
-			System.out.println();
-		}*/
 	}
 
+	public boolean testDigit(Digit d, int k){
+		Cell[][] img = d.getCellArray();
+		double[] probability = new double[10];
+		
+		//transfer priors to new array
+		for(int i = 0 ; i < 10 ; i++){
+			probability[i] = priors[i];
+		}
+
+		for(double p : probability){
+			p = Math.log(p);
+		}
+
+		//need help here
+		for(int digit = 0; digit < 10; digit++ ){
+			for(int i = 0; i < 28; i++){
+				for(int j = 0; j < 28; j++){
+					if(img[i][j].getData()!=0){
+						probability[digit] += Math.log((likelihood[digit][i][j] + k /frequency[digit])) ;
+					}
+				}
+			}
+		}
+
+		//choose the best p
+		int best_p = 0;
+		for(int i = 1; i < 10; i++){
+			if(probability[best_p]<probability[i]){
+				best_p = i;
+			}
+		}
+		d.assign_value(best_p);
+		return d.checkData();
+	}
 }
