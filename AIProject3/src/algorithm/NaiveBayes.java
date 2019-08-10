349854d56086502be2b2e1dbde16e04ff4dabbc7
Charmian Goh <csg102@rutgers.edu>
2019-08-09 23:05:46 -0400

    finished getting all likelihoods ad done laplace smoothing

--- AIProject3/src/algorithm/NaiveBayes.java
+++ AIProject3/src/algorithm/NaiveBayes.java
@@ -1,63 +1,57 @@
 package algorithm;
 
+import java.util.ArrayList;
+
 import objects.Cell;
+import objects.Digit;
 
 public class NaiveBayes {
-	private int[][][] likelihood_empty;
-	private int[][][] likelihood_edge;
-	private int[][][] likelihood_inner;
-	private int[] frequency, priors;
+	private double[][][] likelihood_empty;
+	private double[][][] likelihood_edge;
+	private double[][][] likelihood_inner;
+	private double[] frequency, priors;
+	private ArrayList<Digit> digits;
 
-	public NaiveBayes(int[] quantity, int[] priors){
-		likelihood_empty = new int[10][28][28];
-		likelihood_edge = new int[10][28][28];
-		likelihood_inner = new int[10][28][28];
+	public NaiveBayes(double[] quantity, double[] priors, int k, ArrayList<Digit> digits){
+		//instantiate
+		likelihood_empty = new double[10][28][28];
+		likelihood_edge = new double[10][28][28];
+		likelihood_inner = new double[10][28][28];
 		this.frequency = quantity;
 		this.priors = priors;
-	}
+		this.digits = digits;
 
-	public void likelihood(Cell[][] image, double label_quantity){
-		int k = 1;
-		double[][] empty_grid = new double[image.length][image.length];
-		double[][] inner_grid = new double[image.length][image.length];
-		double[][] edge_grid = new double[image.length][image.length];
-
-		//array is in form 0 = ' ', 1 = '+', 2 = '#'
-		for(int digit = 0; digit < 10; digit++ ){
-			for(int i = 0; i < 28; i++){
-				for(int j = 0; j < 28; j++){
-					if(image[i][j].getData() == 0){
-						likelihood_empty[digit][i][j]++;
-						empty_grid[i][j] = likelihood_empty[digit][i][j];
-					}else if(image[i][j].getData() == 1){
-						likelihood_edge[digit][i][j]++;
-						edge_grid[i][j] = likelihood_edge[digit][i][j];
-					}else{
-						likelihood_inner[digit][i][j]++;
-						inner_grid[i][j] = likelihood_inner[digit][i][j];
+		//get total likelihoods
+		for(int i = 0 ; i < digits.size(); i++){
+			Digit d = digits.get(i);
+			Cell[][] cells = d.getCellArray();
+			for(int j = 0 ; j < cells.length; j++){
+				for(k = 0 ; k < cells.length; k++){
+					switch(cells[j][k].getData()){
+						case 0:
+							likelihood_empty[d.data][i][j]++;
+							break;
+						case 1:
+							likelihood_edge[d.data][i][j]++;
+							break;
+						case 2:
+							likelihood_inner[d.data][i][j]++;
+							break;
 					}
 				}
 			}
 		}
 
+		//laplace smoothing
 		for(int digit = 0; digit < 10; digit++ ){
 			for(int i = 0; i < 28; i++){
 				for(int j = 0; j < 28; j++){
-					//' '
-					if(image[i][j].getData() == 0){
-						double belief = (likelihood_empty[digit][i][j] + k)/(frequency[i] + Math.pow(k, 2));
-						empty_grid[i][j] = belief;
-						//'+'
-					}else if(image[i][j].getData() == 1){
-						double belief = (likelihood_edge[digit][i][j] + k)/(frequency[i] + Math.pow(k, 2));
-						edge_grid[i][j] = belief;
-					}else{
-						//'#'
-						double belief = (likelihood_inner[digit][i][j] + k)/(frequency[i] + Math.pow(k, 2));
-						inner_grid[i][j] = belief;
-					}
+					likelihood_empty[digit][i][j] = (likelihood_empty[digit][i][j] + k)/(frequency[digit]*k*3);
+					likelihood_edge[digit][i][j] = (likelihood_edge[digit][i][j] + k)/(frequency[digit]*k*3);
+					likelihood_inner[digit][i][j] = (likelihood_inner[digit][i][j] + k)/(frequency[digit]*k*3);
 				}
 			}
 		}
 	}
+
 }
